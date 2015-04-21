536c7dc9f5b0eee89795f854a58584674e683213
johanneswuerbach <johannes.wuerbach@googlemail.com>
2015-04-22 00:00:03 +0200

    ember sauce:launch

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,2 @@
 #!/usr/bin/env node
-var path = require('path');
-var spawn = require('child_process').spawn;
-
-var sauciePath = path.join(__dirname, '..', './node_modules/.bin/saucie');
-
-var saucie = spawn(sauciePath, process.argv, { stdio: 'inherit' });
-
-'SIGINT SIGTERM SIGHUP'.split(' ').forEach(function(evt) {
-  process.on(evt, function() {
-    saucie.kill(evt);
-  });
-});
+require('saucie')();
