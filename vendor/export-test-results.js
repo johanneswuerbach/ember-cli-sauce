944daf7d36e1e296978d860f493b77a42361a8b6
Matthew Adams <matthew.adams@q2ebanking.com>
2015-04-27 13:36:56 -0500

    Updated export-test-results.js to remove comment, and add condition to run exportTestResultsForSauce for Mocha after all tests finish

--- vendor/export-test-results.js
+++ vendor/export-test-results.js
@@ -4,7 +4,9 @@ window.global_test_results = null;
 var exportTestResultsForSauce = function(testResults) {
   window.global_test_results = testResults;
 }
-//Prevent those not using QUnit from having Broccoli imports broken
+
 if (typeof QUnit != 'undefined') {
 	QUnit.done(exportTestResultsForSauce);
-};
+} else if (typeof Mocha !== 'undefined') {
+	after(exportTestResultsForSauce);
+}
